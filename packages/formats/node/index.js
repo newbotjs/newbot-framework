46265eccf07119ce9d252a698154ef4a6f89dd64
Samuel Ronce <sam@webcreative5.net>
2018-09-24 14:33:54 +0200

    profile

--- packages/formats/node/index.js
+++ packages/formats/node/index.js
@@ -16,6 +16,47 @@ module.exports = (langDefault) => {
                         session.sendTyping()
                     }  
                 }
+            },
+            profile: {
+                $params: ['data', 'user'],
+                async $call({ session }, user) {
+                    let profile = {}
+                    if (Utils.isBottenderFacebook(session)) {
+                        const ret = await session.context.getUserProfile()
+                        profile = {
+                            name: ret.first_name,
+                            fullname: `${ret.first_name} ${ret.last_name}`,
+                            image: ret.profile_pic,
+                            lang: ret.locale,
+                            gender: ret.gender
+                        }
+                    }
+                    else if (Utils.isBottenderLine(session)) {
+                        const ret = await session.context.getUserProfile()
+                        profile = {
+                            name: ret.name,
+                            image: ret.pictureUrl
+                        }
+                    }
+                    else if (Utils.isBottenderViber(session)) {
+                        const ret = await session.context.getUserDetails()
+                        profile = {
+                            name: ret.name,
+                            image: ret.avatar,
+                            lang: ret.language,
+                            country: ret.country
+                        }
+                    }
+                    else if (Utils.isBottenderTelegram(session)) {
+                        const { from:ret } = session.context.event.inlineQuery()
+                        profile = {
+                            name: ret.first_name,
+                            fullname: `${ret.first_name} ${ret.last_name}`,
+                            lang: ret.language_code
+                        }
+                    }
+                    user.setMagicVariable('profile', profile)
+                }
             }
         }
     }

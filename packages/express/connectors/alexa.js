90de77e112402215332bc2c5200bf8c939a7451f
Samuel Ronce <sam@webcreative5.net>
2019-08-22 14:13:18 +0200

    alexa connector improved

--- packages/express/connectors/alexa.js
+++ packages/express/connectors/alexa.js
@@ -16,15 +16,44 @@ module.exports = function ({
         checkCert: isProd
     })
 
-    const exec = (req, res) => {
-        const session = new Session(req, res)
-        const { userId } = req.data.session
-        const text = req.slot('any')
-        const _converse = global.converse || converse
-        return _converse.exec(text, userId, output(session, settings))
+    const exec = (req, res, eventName) => {
+        try {
+            const session = new Session(req, res)
+            const {
+                userId
+            } = req.data.session
+            const text = req.slot('any')
+            const _converse = global.converse || converse
+            const _settings = output(session, settings)
+            if (eventName) {
+                return _converse.event(eventName, [userId], _settings)
+            }
+            return _converse.exec(text, userId, _settings)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    alexaApp.launch(exec)
-    alexaApp.intent('AMAZON.StopIntent', exec)
+    const launch = (req, res) => {
+        return exec(req, res, 'start')
+    }
+
+    alexaApp.pre = (req, res, type) => {
+        try {
+            const name = _.get(req, 'data.request.intent.name')
+            if (name && /^AMAZON/.test(name)) {
+                req.eventName = name
+                _.set(req, 'data.request.intent.name', 'NewBotEvent')
+                return 
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    alexaApp.launch(launch)
     alexaApp.intent("NewBotIntent", exec)
+    alexaApp.intent("NewBotEvent", (req, res) => {
+        return exec(req, res, req.eventName)
+    })
 }

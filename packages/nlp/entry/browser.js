9d9986bcc2516801a2b4bb871399a5d5c5cc122e
Samarium <sam@webcreative5.net>
2020-02-12 19:12:33 +0300

    remove decrepated packages

--- packages/nlp/entry/browser.js
+++
@@ -1,8 +0,0 @@
-import processNlp from '../src/process'
-
-export default async function(path) {
-    const manager = new window.NLPJS.NlpManager()
-    const model = await fetch(path).then(res => res.json())
-    manager.import(model)
-    return processNlp(manager)
-}

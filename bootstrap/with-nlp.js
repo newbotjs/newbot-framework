40a6a94b098c86b463395bf99cce7aad5240de63
Samarium <sam@webcreative5.net>
2020-02-18 13:05:25 +0300

    extract entities

--- bootstrap/with-nlp.js
+++ bootstrap/with-nlp.js
@@ -1,5 +1,6 @@
 const { containerBootstrap } = require('@nlpjs/core')
 const { Nlp } = require('@nlpjs/nlp')
+const { BuiltinMicrosoft } = require('@nlpjs/builtin-microsoft')
 const processNlp = require('../src/nlp/native')
 const Converse = require('../src/converse')
 const browser = require('../src/utils/browser')
@@ -7,11 +8,13 @@ const browser = require('../src/utils/browser')
 module.exports = (fs) => {
     Converse.nlpManager = async function(path, langs = []) {
         const container = await containerBootstrap()
-        container.use(Nlp);
+        container.use(Nlp)
+        const manager = container.get('nlp')
+
         for (let lang of langs) {
             container.use(lang)
         }
-        const manager = container.get('nlp')
+
         if (path[0] == '{') {
             manager.import(path)
         }
@@ -23,6 +26,10 @@ module.exports = (fs) => {
             const model = fs.readFileSync(path, 'utf-8')
             manager.import(model)
         }
+
+        const builtin = new BuiltinMicrosoft()
+        container.register('extract-builtin-??', builtin, true)
+
         return processNlp(manager)
     }
     return Converse
--- src/nlp/native.js
+++ src/nlp/native.js
@@ -1,6 +1,6 @@
 module.exports = function processNlp(manager) {
     return async (text, userId, converse) => {
-        
+
         const result = await manager.process(text)
 
         const taln = {}
